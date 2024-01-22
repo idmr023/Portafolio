a5ee999cdddb845a1af63913037f10f78995fc4e
Ivan Manrique <jparatupcya@gmail.com>
2024-01-22 08:52:47 -0500

    Errores solucionados

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
@@ -13,4 +14,5 @@ export default defineConfig({
     }
   },
   output: "server",
+  adapter: vercel()
 });
